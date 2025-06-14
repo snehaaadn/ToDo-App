cf7ecefe1c04513ea9703d144e2e450e41cb56a3
snehaaadn <155845591+snehaaadn@users.noreply.github.com>
2025-06-14 21:16:59 +0530

    Added the tests folder

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Sneha Debnath on 14/10/24.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
